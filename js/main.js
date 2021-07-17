e88984844e8327a3b3993ea4dbd3f8cb2f9628e4
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-07-17 13:24:04 -0700

    Completed issue-4-optional-user-can-choose-the-rate/review-at-home-screen.

--- js/main.js
+++ js/main.js
@@ -13,25 +13,21 @@ const modal = document.querySelector('.blackBox');
 const modalTitle = document.querySelector('.h3');
 const next = document.querySelector('.next');
 const favoriteButton = document.querySelector('.favoriteHeart');
-const ul = document.querySelector('ul');
+const myFavoritesUl = document.querySelector('.myFavoritesUl');
 const myFavorites = document.querySelector('.myFavorites');
 const heartIcon = document.querySelector('.heartIcon');
+const reviewAnimesButton = document.querySelector('.reviewAnimes');
+const reviewsUl = document.querySelector('.reviewsUl');
+const reviewAnimesSection = document.querySelector('.reviewAnimesSection');
 
 document.addEventListener('click', handleClicks);
 
 function handleClicks(event) {
   const buttonPressed = event.target.className;
   if (buttonPressed === houseIcon.className) {
-    animeTriviaViewParagraph.textContent = '';
-    h1.textContent = 'Anime Fans';
-    homeScreen.classList.remove('hidden');
-    animeTriviaView.classList.add('hidden');
-    myFavorites.classList.add('hidden');
+    handleHouseIcon();
   } else if (buttonPressed === animeTriviaButton.className) {
-    handleApi();
-    h1.textContent = 'Anime Trivia';
-    animeTriviaView.classList.remove('hidden');
-    homeScreen.classList.add('hidden');
+    handleAnimeTriviaButton();
   } else if (buttonPressed === animeTriviaSubmit.className) {
     handleAnimeTriviaSubmit();
   } else if (buttonPressed === next.className) {
@@ -39,10 +35,9 @@ function handleClicks(event) {
   } else if (buttonPressed === favoriteButton.className) {
     handleFavoriteButton();
   } else if (buttonPressed === heartIcon.className) {
-    h1.textContent = 'My Favorites';
-    homeScreen.classList.add('hidden');
-    animeTriviaView.classList.add('hidden');
-    myFavorites.classList.remove('hidden');
+    handleHeartIcon();
+  } else if (buttonPressed === reviewAnimesButton.className) {
+    handleReviewAnimesButton();
   }
 }
 
@@ -57,9 +52,15 @@ function handleApi() {
     animeTriviaViewParagraph.textContent = xhr.response.quote;
     correctAnime.textContent = xhr.response.anime;
     correctCharacter.textContent = xhr.response.character;
-
   }
   xhr.send();
+}
+
+function handleAnimeTriviaButton() {
+  handleApi();
+  h1.textContent = 'Anime Trivia';
+  animeTriviaView.classList.remove('hidden');
+  homeScreen.classList.add('hidden');
 }
 
 function handleAnimeTriviaSubmit() {
@@ -67,31 +68,65 @@ function handleAnimeTriviaSubmit() {
     modal.classList.remove('hidden');
     modalTitle.textContent = 'Correct!';
     modalTitle.classList.add('correct');
-
   } else {
     modal.classList.remove('hidden');
     modalTitle.textContent = 'inCorrect!';
     modalTitle.classList.remove('correct');
     modalTitle.classList.add('incorrect');
-
   }
-
 }
 
+function handleHouseIcon() {
+  animeTriviaViewParagraph.textContent = '';
+  reviewsUl.innerHTML = '';
+  h1.textContent = 'Anime Fans';
+  homeScreen.classList.remove('hidden');
+  animeTriviaView.classList.add('hidden');
+  myFavorites.classList.add('hidden');
+}
 function handleNextButton() {
   handleApi();
   modal.classList.add('hidden');
   animeCharacter.value = '';
   animeName.value = '';
 }
-
 function handleFavoriteButton() {
   h1.textContent = 'My Favorites';
   const li = document.createElement('li');
   li.textContent = correctAnime.textContent;
-  ul.appendChild(li);
+  myFavoritesUl.appendChild(li);
   modal.classList.add('hidden');
   homeScreen.classList.add('hidden');
   animeTriviaView.classList.add('hidden');
   myFavorites.classList.remove('hidden');
+}
+function handleHeartIcon() {
+  h1.textContent = 'My Favorites';
+  homeScreen.classList.add('hidden');
+  animeTriviaView.classList.add('hidden');
+  myFavorites.classList.remove('hidden');
+}
+function handleReviewAnimesButton() {
+  h1.textContent = 'Review Animes';
+  reviewAnimesSection.classList.remove('hidden');
+  homeScreen.classList.add('hidden');
+  animeTriviaView.classList.add('hidden');
+  myFavorites.classList.add('hidden');
+  for (let i = 0; i < 10; i++) {
+    handleReviewsApi();
+  }
+}
+function handleReviewsApi() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://animechan.vercel.app/api/random');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', handleXhr);
+  function handleXhr() {
+    // console.log(xhr.status);
+    // console.log(xhr.response);
+    const li = document.createElement('li');
+    li.textContent = xhr.response.anime;
+    reviewsUl.appendChild(li);
+  }
+  xhr.send();
 }
